c15b70ad1eca57023c2384b8920b53bb40e4e896
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-02-28 01:24:53 +0530

    add toggle btn

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.getElementById("ageInput").addEventListener("input", function (e) {
 document.getElementById("bioInput").addEventListener("input", function (e) {
   document.getElementById("bioPreview").textContent = e.target.value;
 });
+let btn = document.getElementById("resetButton");
+let body = document.body;
+btn.addEventListener("click", function () {
+  if (body.style.background === "linear-gradient(to right, #1d2671, #c33764") {
+    body.style.background = "linear-gradient(to right, #6a11cb, #2575fc)";
+  } else {
+    body.style.background = "linear-gradient(to right, #1d2671, #c33764)";
+  }
+});
